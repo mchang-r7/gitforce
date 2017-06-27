636347e35a51b14ca1af786d21d185832eafb641
Marty Chang <maty.y.chang@gmail.com>
2017-06-27 17:15:13 -0400

    fancy button

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Welcome to the app.js project!')
+  res.send('Welcome to the app.js project!'
+      + '<button onclick="alert(\'hello\')">Click Me</button>')
 })
 
 app.listen(3000, function () {
