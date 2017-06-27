1eadd2e33ccf2afec2a0ec6a912379ab291812d8
asmorodin-r7 <asmorodin@rapid7.com>
2017-06-27 17:09:26 -0400

    Updated welcome messge on the app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Welcome to the app.js project!')
 })
 
 app.listen(3000, function () {
